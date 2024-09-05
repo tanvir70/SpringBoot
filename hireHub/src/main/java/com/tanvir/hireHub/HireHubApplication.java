5e0addbbbb5626bc22c0098abce00feac20e1c36
tanvir70 <rifattanvir70@gmail.com>
2024-09-05 08:45:42 +0600

    Initial commit

---
+++ hireHub/src/main/java/com/tanvir/hireHub/HireHubApplication.java
@@ -0,0 +1,13 @@
+package com.tanvir.hireHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireHubApplication.class, args);
+	}
+
+}
