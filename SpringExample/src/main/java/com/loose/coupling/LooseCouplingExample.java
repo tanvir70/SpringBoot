07747dc9490e19a7074a6e3e9886fff98cde6053
tanvir70 <rifattanvir70@gmail.com>
2024-10-17 12:09:08 +0600

    Leaning core spring concepts: DI, IOC, bean

--- SpringExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ SpringExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,17 +1,31 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(String[] args) {
-      UserDataProvider databaseProvider = new UserDatabaseProvider();
-      UserManager userManagerWithDB = new UserManager(databaseProvider);
+//      UserDataProvider databaseProvider = new UserDatabaseProvider();
+//      UserManager userManagerWithDB = new UserManager(databaseProvider);
+//      System.out.println(userManagerWithDB.getUserInfo());
+//
+//      UserDataProvider webServiceProvider = new WebServiceDataProvider();
+//      UserManager userManagerWithWebService = new UserManager(webServiceProvider);
+//      System.out.println(userManagerWithWebService.getUserInfo());
+//
+//      NewDatabaseProvider newDatabaseProvider = new NewDatabaseProvider();
+//      UserManager userManagerWithNewDatabase = new UserManager(newDatabaseProvider);
+//      System.out.println(userManagerWithNewDatabase.getUserInfo());
+
+      ApplicationContext context = new ClassPathXmlApplicationContext("applicationLooseCoupling.xml");
+
+      UserManager userManagerWithDB = (UserManager) context.getBean("userManagerWithUDP");
       System.out.println(userManagerWithDB.getUserInfo());
 
-      UserDataProvider webServiceProvider = new WebServiceDataProvider();
-      UserManager userManagerWithWebService = new UserManager(webServiceProvider);
+      UserManager userManagerWithWebService = (UserManager) context.getBean("userManagerWithWSDP");
       System.out.println(userManagerWithWebService.getUserInfo());
 
-      NewDatabaseProvider newDatabaseProvider = new NewDatabaseProvider();
-      UserManager userManagerWithNewDatabase = new UserManager(newDatabaseProvider);
+      UserManager userManagerWithNewDatabase = (UserManager) context.getBean("userManagerWithNDP");
       System.out.println(userManagerWithNewDatabase.getUserInfo());
     }
 }
