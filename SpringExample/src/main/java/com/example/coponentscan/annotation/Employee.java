b1309ef4681eaf8165a27e577d216e30043be53b
tanvir70 <rifattanvir70@gmail.com>
2024-10-20 22:06:50 +0600

    Learnt component scan with xml & without xml file

---
+++ SpringExample/src/main/java/com/example/coponentscan/annotation/Employee.java
@@ -0,0 +1,57 @@
+package com.example.coponentscan.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    private int employeeId;
+    @Value("Tanvir Rifat")
+    private String firstName;
+    @Value("${java.home}")
+    private String lastName;
+    @Value("#{5 * 5}")
+    private String salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+}
