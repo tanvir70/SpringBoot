3fe91180a219462be7f24ce1c143da5165965c7c
tanvir70 <rifattanvir70@gmail.com>
2024-10-21 18:40:39 +0600

    Starting Spring Boot learnings

---
+++ SpringBootFirstApplication/src/test/java/com/tanvir/SpringBootFirstApplication/SpringBootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvir.SpringBootFirstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringExample/src/main/java/com/example/autowired/annotation/App.java
@@ -0,0 +1,12 @@
+package com.example.coponentscan.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee employee = (Employee) context.getBean("employee");
+        System.out.println(employee);
+    }
+}
